8d69446c49860c64ea7a56796eeabd5bbb49c8a8
Christian Braune <christian.braune@noreply.gitlab.com>
2026-04-19 10:38:24 +0200

    Initial Commit

---
+++ src/main/java/demo/validator/XmlXsdValidatorApplication.java
@@ -0,0 +1,13 @@
+package demo.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlXsdValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlXsdValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/validator/XmlXsdValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlXsdValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
